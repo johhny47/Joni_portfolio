c0e5084f550d426e3ae6f2bcf453a48cd172a916
johhny47 <mdjohnny063@gmail.com>
2025-02-07 11:28:00 +0600

    add readme

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
           <div className="max-w-4xl mx-auto px-6 text-center">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">About Me</h2>
             <p className="text-sm md:text-lg text-gray-500 font-bold">
-              Hello, I'm Joni Fakir. I'm a developer  experience with dynamic, user-centric websites and applications. I have a strong proficiency in front-end technologies such as HTML, CSS, JavaScript, and React frameworks. On the back-end, I work with Node.js, Expressjs and MongoDB as databases.
-              I am passionate about clean code, responsive design, and optimizing web performance. My expertise also extends to version control (Git), APIs, and ensuring that I deliver high-quality projects on time and within budget.
+            Hi, I’m Joni Fakir, a passionate web developer with a focus on React.js, Node.js, Express.js, and MongoDB. I started my programming journey a year ago and love working on projects that solve real-world problems and make a meaningful impact. Always eager to learn, I thrive on building web applications and optimizing systems. Outside of coding, I’m an avid cricket and football fan, and enjoy running to recharge. I value collaboration, approach projects with a positive attitude, and am dedicated to delivering great results. If you’re looking for a driven developer, let’s connect!
+
             </p>
           </div>
         </section>
